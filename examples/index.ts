5e427cadf43a6212990815da99cd172ef47dcf96
Michael Cavanaugh <funkel1989@gmail.com>
2021-05-05 19:10:38 -0400

    working through examples

---
+++ examples/index.ts
@@ -0,0 +1,12 @@
+// build example
+import { SqsLargePayloadService, ISqsServiceOptions, ISqsLargePayloadService } from '@battleline/sqs-large-payload-nodejs/src';
+
+const sqsOptions: ISqsServiceOptions = {
+    s3EndpointUrl: 'string',
+    s3BucketName: 'string',
+    region: 'us-east-2'
+};
+
+const sqs: ISqsLargePayloadService = new SqsLargePayloadService(sqsOptions);
+
+sqs
